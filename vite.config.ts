cce0eab09785b9a983d37f30f3d3c0ff533885c6
Angel Gabriel Lopez <angel178293lopez@gmail.com>
2024-01-24 21:56:40 -0400

    Add TSR configuration and TanStack Router Vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import react from '@vitejs/plugin-react-swc'
 import million from 'million/compiler'
 import path from 'path'
+import { TanStackRouterVite } from '@tanstack/router-vite-plugin'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  plugins: [million.vite({ auto: true }), react()],
+  plugins: [million.vite({ auto: true }), TanStackRouterVite(), react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
