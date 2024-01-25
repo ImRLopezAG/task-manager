cec1818ad78be42c11c6f21898725bce4d12e772
Angel Gabriel Lopez <angel178293lopez@gmail.com>
2024-01-24 21:57:42 -0400

    Add TanStack Router and routeTree.gen.ts

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { App } from '@app/app'
-import { ThemeProvider } from '@app/providers'
+import { RouterProvider, Router } from '@tanstack/react-router'
+import { routeTree } from './routeTree.gen'
 
 import './index.css'
 
+const router = new Router({
+  routeTree,
+  defaultPreload: 'intent'
+})
+
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router
+  }
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
+    <RouterProvider router={router} />
   </React.StrictMode>
 )
